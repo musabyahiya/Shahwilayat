c5f2c7e53afb75f907c1bccab6288adf4c116ea3
Musab Yahiya <musabyahiya@hotmail.com>
2019-07-18 17:16:32 +0500

    Add project files.

---
+++ ShahWilayat/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShahWilayat.Startup))]
+namespace ShahWilayat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
