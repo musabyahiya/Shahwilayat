1460106ac2538684e5a80be9e6acb1b1bd4df5a9
Musab Yahiya <musabyahiya@hotmail.com>
2019-10-05 17:12:38 +0500

    Transfer Order integrated

--- ShahWilayat/Scripts/assets/js/PagesJS/Transfer.js
+++ ShahWilayat/Scripts/assets/js/PagesJS/Transfer.js
@@ -263,3 +263,33 @@ function EditTransfer(selector) {
     $('.txtTransferDate_upd').val(objEditRow.find('.tdTransferDate').text());
     $('.txtTransferOrderNo_upd').val(objEditRow.find('.tdTransferOrderNo').text());
 }
+
+
+function PrintTransferOrder() {
+    $(function () {
+        //$('#Print').load('http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js');
+
+    });
+
+    var contents = $("#PrintTransferOrder").html();
+    var frame1 = $('<iframe />');
+    frame1[0].name = "frame1";
+    frame1.css({ "position": "absolute", "top": "-1000000px" });
+    $("body").append(frame1);
+    var frameDoc = frame1[0].contentWindow ? frame1[0].contentWindow : frame1[0].contentDocument.document ? frame1[0].contentDocument.document : frame1[0].contentDocument;
+    frameDoc.document.open();
+    //Create a new HTML document.
+    frameDoc.document.write('<html><head><title>Membership Single</title>');
+    frameDoc.document.write('</head><body>');
+    //Append the external CSS file.
+    frameDoc.document.write('<link href="/Content/assets/css/PrintMaterial/TransferOrder/TransferOrder.css" rel="stylesheet" type="text/css" />');
+    //Append the DIV contents.
+    frameDoc.document.write(contents);
+    frameDoc.document.write('</body></html>');
+    frameDoc.document.close();
+    setTimeout(function () {
+        window.frames["frame1"].focus();
+        window.frames["frame1"].print();
+        frame1.remove();
+    }, 700);
+}
