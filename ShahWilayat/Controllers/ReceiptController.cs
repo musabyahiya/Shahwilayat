4b170435839d920a3fea64bfbc8f7c9eff3eb9ed
Musab Yahiya <musabyahiya@hotmail.com>
2019-10-08 18:41:14 +0500

    Done with Receipt integeration.

--- ShahWilayat/Controllers/ReceiptController.cs
+++ ShahWilayat/Controllers/ReceiptController.cs
@@ -12,7 +12,7 @@ namespace ShahWilayat.Controllers
     [SessionCheck]
     public class ReceiptController : Controller
     {
-       
+
         ShahWilayatEntities context = new ShahWilayatEntities();
         // GET: Receipt
         public ActionResult Index()
@@ -20,18 +20,48 @@ namespace ShahWilayat.Controllers
             return View();
         }
 
-        public string GetAllReceipt()
+        public string GetPaidReceipt(string ReceiptNo)
         {
-            DataSet ds = new DataSet();
-            DataTable dt = new DataTable();
-            string dbConnectionString = context.Database.Connection.ConnectionString;
-            SqlConnection con = new SqlConnection(dbConnectionString);
-            SqlDataAdapter da = new SqlDataAdapter("GetPaidReceipt", con);
-            da.SelectCommand.CommandType = CommandType.StoredProcedure;
-            da.SelectCommand.Parameters.Add("@ReceiptNo", SqlDbType.VarChar).Value = "947";
-            da.Fill(dt);
+            try
+            {
+                DataSet ds = new DataSet();
+                DataTable dt = new DataTable();
+                string dbConnectionString = context.Database.Connection.ConnectionString;
+                SqlConnection con = new SqlConnection(dbConnectionString);
+                SqlDataAdapter da = new SqlDataAdapter("GetPaidReceipt", con);
+                da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                da.SelectCommand.Parameters.Add("@ReceiptNo", SqlDbType.VarChar).Value = ReceiptNo;
+                da.Fill(dt);
 
-            return JsonConvert.SerializeObject(dt);
+                return JsonConvert.SerializeObject(dt);
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+
+        }
+
+        public string GetAllReceipts()
+        {
+            try
+            {
+                DataSet ds = new DataSet();
+                DataTable dt = new DataTable();
+                string dbConnectionString = context.Database.Connection.ConnectionString;
+                SqlConnection con = new SqlConnection(dbConnectionString);
+                SqlDataAdapter da = new SqlDataAdapter("GetAllReceipts", con);
+                da.SelectCommand.CommandType = CommandType.StoredProcedure;
+
+                da.Fill(dt);
+
+                return JsonConvert.SerializeObject(dt);
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+
         }
 
     }
--- ShahWilayat/Scripts/assets/js/PagesJS/Receipt.js
+++ ShahWilayat/Scripts/assets/js/PagesJS/Receipt.js
@@ -1,8 +1,12 @@
-﻿GetAllReceipt();
+﻿GetAllReceipts();
+
+
+
 AllClickFunction();
 SearchTable();
 var ReceiptList;
-
+var PaidReceiptList;
+var ReceiptNo;
 var objEditRow;
 
 
@@ -14,13 +18,13 @@ function SearchTable() {
         var search = $(_this).val();
 
         if (search == '') {
-            onGetAllReceipt(PlotList);
+            onGetAllReceipts(ReceiptList);
         }
         else {
             var obj = ReceiptList.filter(x=> x.ReceiptNo.includes(search()) ||
 				x.PlotNo.includes(search)
 				)
-            onGetAllReceipt(obj);
+            onGetAllReceipts(obj);
 
         }
         ProgressBarHide();
@@ -29,9 +33,9 @@ function SearchTable() {
 function AllClickFunction() {
 
 
-    $('.btnGenerateReceipt').click(function () {
-        ProgressBarShow();
-        var contents = $("#Print").html();
+    $(".btnGenerateReceipt").click(function () {
+
+        var contents = $("#PrintReceipt").html();
         var frame1 = $('<iframe />');
         frame1[0].name = "frame1";
         frame1.css({ "position": "absolute", "top": "-1000000px" });
@@ -41,9 +45,8 @@ function AllClickFunction() {
         //Create a new HTML document.
         frameDoc.document.write('<html><head><title>Receipt</title>');
         frameDoc.document.write('</head><body>');
-
         //Append the external CSS file.
-        frameDoc.document.write('<link href="../Content/assets/css/PrintMaterial/Receipt.css" rel="stylesheet" type="text/css" />');
+        frameDoc.document.write('<link href="/Content/assets/css/PrintMaterial/Receipt/Receipt.css" rel="stylesheet" type="text/css" />');
         //Append the DIV contents.
         frameDoc.document.write(contents);
         frameDoc.document.write('</body></html>');
@@ -52,24 +55,22 @@ function AllClickFunction() {
             window.frames["frame1"].focus();
             window.frames["frame1"].print();
             frame1.remove();
-        }, 600);
-        ProgressBarHide();
+        }, 1000);
     });
+
 }
 
 
-
-
-function GetAllReceipt() {
+function GetPaidReceipt(ReceiptNo) {
     ProgressBarShow();
     var request = $.ajax({
         method: "POST",
-        url: "/Receipt/GetAllReceipt",
-        data: {}
+        url: "/Receipt/GetPaidReceipt",
+        data: { ReceiptNo: ReceiptNo }
     });
     request.done(function (data) {
 
-        onGetAllReceipt(data);
+        onGetPaidReceipt(data);
     });
     request.fail(function (jqXHR, Status) {
         console.log(jqXHR.responseText);
@@ -77,7 +78,78 @@ function GetAllReceipt() {
     });
 }
 
-function onGetAllReceipt(data) {
+function onGetPaidReceipt(data) {
+
+    try {
+        var res = JSON.parse(data)
+        PaidReceiptList = res;
+
+        // Binding Receipt
+        var obj = PaidReceiptList.filter(x=> x.ReceiptNo == ReceiptNo);
+        var TotalAmount = 0;
+        $.each(obj, function () {
+            TotalAmount += parseFloat(this.PaymentAmount);
+        });
+
+        BindTextToSelector('.printIssueDate', GetCurrentDatePakFormat());
+        BindTextToSelector('.printReceiptNo', obj[0].ReceiptNo);
+        BindTextToSelector('.printMembershipNo', obj[0].MembershipNo);
+        BindTextToSelector('.printPaymentDate', formatDatePakFormat(obj[0].PaymentDate));
+        BindTextToSelector('.printPlotNo', obj[0].PlotNo);
+        BindTextToSelector('.printReceiptNo', obj[0].ReceiptNo);
+        BindTextToSelector('.printTitle', 'Received from ' + obj[0].Title + ': ');
+        BindTextToSelector('.printFullName', obj[0].FullName);
+        BindTextToSelector('.printMemberRelation', obj[0].MemberRelation + ': ');
+        BindTextToSelector('.printReceiptNo', obj[0].ReceiptNo);
+        BindTextToSelector('.printFatherName', obj[0].FatherName);
+
+        BindTextToSelector('.printAmount', moneyFormat(TotalAmount) + '/= (' + GetAmountInWords(TotalAmount).capitalize() + ')');
+        BindTextToSelector('.printPaymentMethod', 'by ' + obj[0].PaymentMethod + ':');
+        BindTextToSelector('.printChequeNo', obj[0].ChequeNo);
+        BindTextToSelector('.printChequeDate', formatDatePakFormat(obj[0].ChequeDate));
+        BindTextToSelector('.printPlotType', 'on account of ' + obj[0].PlotType + ' Plot of:');
+        BindTextToSelector('.printSize', obj[0].Size.toFixed(2));
+        BindTextToSelector('.printTotalAmount', moneyFormat(TotalAmount) + '/-');
+
+
+        var divTbodyGoalFund = $(".ReceiptEntityListing").html("");
+        $("#ReceiptEntityListing").tmpl(obj).appendTo(divTbodyGoalFund);
+
+        var i = 1;
+        $('.trReceiptEntity').each(function () {
+            $(this).find('span').first().text(i);
+            i++;
+        });
+
+
+        PrintExtraRows(obj.length);
+        // Binding Receipt
+        ProgressBarHide();
+    }
+    catch (Err) {
+        console.log(Err);
+    }
+    ProgressBarHide();
+}
+
+function GetAllReceipts() {
+    ProgressBarShow();
+    var request = $.ajax({
+        method: "POST",
+        url: "/Receipt/GetAllReceipts",
+        data: {}
+    });
+    request.done(function (data) {
+
+        onGetAllReceipts(data);
+    });
+    request.fail(function (jqXHR, Status) {
+        console.log(jqXHR.responseText);
+
+    });
+}
+
+function onGetAllReceipts(data) {
 
     try {
         var res = JSON.parse(data)
@@ -90,7 +162,7 @@ function onGetAllReceipt(data) {
             $(this).find('td').first().text(i);
             i++;
         });
-
+        SearchTable();
         ProgressBarHide();
     }
     catch (Err) {
@@ -100,47 +172,29 @@ function onGetAllReceipt(data) {
 }
 function EditReceipt(selector) {
     objEditRow = $(selector).closest('tr');
-    var ReceiptNo = objEditRow.find('.hdnReceiptNo').val();
+     ReceiptNo = objEditRow.find('.hdnReceiptNo').val();
 
-    var obj = ReceiptList.filter(x=> x.ReceiptNo == ReceiptNo);
-    var TotalAmount = 0;
-    $.each(obj, function () {
-        TotalAmount += parseFloat(this.PaymentAmount);
-    });
-
-    BindTextToSelector('.printIssueDate', GetCurrentDatePakFormat());
-    BindTextToSelector('.printReceiptNo', obj[0].ReceiptNo);
-    BindTextToSelector('.printMembershipNo', obj[0].MembershipNo);
-    BindTextToSelector('.printPaymentDate', formatDatePakFormat(obj[0].PaymentDate));
-    BindTextToSelector('.printPlotNo', obj[0].PlotNo);
-    BindTextToSelector('.printReceiptNo', obj[0].ReceiptNo);
-    BindTextToSelector('.printTitle', obj[0].Title + ': ' + obj[0].FullName);
-    BindTextToSelector('.printMemberRelation', obj[0].MemberRelation + ': ' + obj[0].FatherName);
-    BindTextToSelector('.printReceiptNo', obj[0].ReceiptNo);
-    BindTextToSelector('.printFatherName', obj[0].FatherName);
-
-    BindTextToSelector('.printAmount', moneyFormat(TotalAmount) + '/= (' + GetAmountInWords(TotalAmount).capitalize() + ')');
-    BindTextToSelector('.printPaymentMethod', 'by ' + obj[0].PaymentMethod + ':');
-    BindTextToSelector('.printChequeNo', obj[0].ChequeNo);
-    BindTextToSelector('.printChequeDate', formatDatePakFormat(obj[0].ChequeDate));
-    BindTextToSelector('.printPlotType', obj[0].PlotType);
-    BindTextToSelector('.printSize', obj[0].Size.toFixed(2));
-    BindTextToSelector('.printTotalAmount', moneyFormat(TotalAmount) + '/-');
-
-
-    var divTbodyGoalFund = $(".ReceiptEntityListing").html("");
-    $("#ReceiptEntityListing").tmpl(obj).appendTo(divTbodyGoalFund);
-
-    var i = 1;
-    $('.trReceiptEntity').each(function () {
-        $(this).find('td').first().text(i);
-        i++;
-    });
-
+    GetPaidReceipt(ReceiptNo);
 
 
 }
 
+function PrintExtraRows(Lenght) {
+    var tblRows = '';
+    var InitialPoint = $('.trReceiptEntity').length + 1;
+    for (var i = 0; i < 16 - Lenght; i++) {
+        tblRows += '<tr style="line-height:1.3;" >';
 
+        tblRows += '<td style="text-align: center;"><span>' + InitialPoint + '</span></td>';
+        tblRows += '<td><span>__________________</span></td>';
+
+        tblRows += '<td style="text-align: center;"><span>_______</span></td>';
+        tblRows += '</tr>';
+        InitialPoint++;
+    }
+
+    $('.trReceiptEntity:last').after(tblRows);
+
+}
 
 
